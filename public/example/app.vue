ba2aaf58184f52ea50ff7dea87e4ef0ba0f429f1
张猛(zhangmeng04) <zhangmeng04@longfor.com>
2021-01-08 18:57:19 +0800

    tianjia

---
+++ public/example/app.vue
@@ -0,0 +1,21 @@
+<template>
+  <p class="filter"></p>
+</template>
+
+<script>
+  export default {
+    created() {
+      var listener = createCopyListener('zhoujiang1', '123456')
+      listener.addRule('className', 'IDName')
+      listener.connect().then(transfer => {
+        // store transfer
+      })
+    }
+  }
+</script>
+
+<style lang='stylus'>
+  body {
+    font-family: 'longhu-font' "Microsoft YaHei", "Helvetica Neue", Helvetica, STHeiTi, Arial, sans-serif
+  }
+</style>
